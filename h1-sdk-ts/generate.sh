818b5b84e088ccde634a41cec568c56b20d35e8e
kkm <kubasurdej@gmail.com>
2020-09-24 01:24:28 +0200

    Return to typescript-axios generator

--- h1-sdk-ts/generate.sh
+++ h1-sdk-ts/generate.sh
@@ -1,4 +1,1 @@
-openapi-generator generate -i .\\openapi.json -c config.yaml -g javascript
-yarn add -D typescript@4.0.2
-yarn tsc
-yarn build
+openapi-generator generate -i .\\openapi.json -c config.yaml -g typescript-axios

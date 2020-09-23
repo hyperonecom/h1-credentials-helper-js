aba923139931fed3aa64b91ea49d8725bb27cca7
kkm <kubasurdej@gmail.com>
2020-09-24 01:43:07 +0200

    Generate documentation from type declarations

--- h1-sdk-ts/generate.sh
+++ h1-sdk-ts/generate.sh
@@ -1,1 +1,4 @@
 openapi-generator generate -i .\\openapi.json -c config.yaml -g typescript-axios
+yarn add -D typedoc typedoc-plugin-markdown # todo: pin version
+yarn typedoc --plugin typedoc-plugin-markdown
+yarn build

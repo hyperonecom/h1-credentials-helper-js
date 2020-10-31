307e97a122a4feb926a0b86607feb2f2751173cf
kkm <kubasurdej@gmail.com>
2020-10-31 16:16:56 +0100

    Add types to jest config

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+};
+
+export default config;
